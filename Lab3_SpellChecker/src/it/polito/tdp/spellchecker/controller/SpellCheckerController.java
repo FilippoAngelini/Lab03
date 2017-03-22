e424bc3ad3f63f49468c1c7075af128f4ac75ed0
212345 <212345@lepcopi43.leplab.polito.it>
2017-03-22 11:04:22 +0100

    Finito punto 1

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,112 @@
+/**
+ * Sample Skeleton for 'SpellChecker.fxml' Controller Class
+ */
+
 package it.polito.tdp.spellchecker.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class SpellCheckerController {
 	
+	Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="cmbLanguage"
+    private ComboBox<String> cmbLanguage; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtInsert"
+    private TextArea txtInsert; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCheck"
+    private Button btnCheck; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblNumber"
+    private Label lblNumber; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnClear"
+    private Button btnClear; // Value injected by FXMLLoader
+
+    @FXML // fx:id="lblTime"
+    private Label lblTime; // Value injected by FXMLLoader
+
+    @FXML
+    void doCheck(ActionEvent event) {
+    	
+    	if(cmbLanguage.getValue() == null)
+    		return;
+    	
+    	if(txtInsert.getText().compareTo("")==0){
+    		txtResult.setText("Devi inserire del testo!");
+    		return;
+    	}
+    	
+    	/*
+    	String testo = txtInsert.getText().replaceAll("[ \\p{Punct}]", " ");
+    	testo = txtInsert.getText().replaceAll(" +", " ");
+    	*/
+    	
+    	model.loadDictionary(cmbLanguage.getValue());
+    	
+    	Long t1 = System.nanoTime();
+    	
+    	String risultato = model.controllaTesto(txtInsert.getText());
+    	
+    	Long t2 = System.nanoTime();
+    	
+    	txtResult.setText(risultato);
+    	lblNumber.setText("The text contains " + model.getNumeroErrori() + " errors");
+    	lblNumber.setVisible(true);
+    	
+    	lblTime.setText("Spell check completed in " + ((t2-t1)/1e9) + " seconds");
+    	lblTime.setVisible(true);
+
+    }
+
+    @FXML
+    void doClear(ActionEvent event) {
+    	
+    	txtInsert.clear();
+    	txtResult.clear();
+    	lblNumber.setVisible(false);
+    	lblTime.setVisible(false);
+
+    }
+    
+    public void setModel(Model model){
+    	this.model = model;
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert cmbLanguage != null : "fx:id=\"cmbLanguage\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnCheck != null : "fx:id=\"btnCheck\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblNumber != null : "fx:id=\"lblNumber\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'SpellChecker.fxml'.";
+        
+        cmbLanguage.getItems().addAll("English","Italian");
+        if(cmbLanguage.getItems().size() > 0)
+        	cmbLanguage.setValue(cmbLanguage.getItems().get(0));
+        lblNumber.setVisible(false);
+    	lblTime.setVisible(false);
+    }
 }
--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Model.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Model.java
@@ -4,19 +4,24 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Model {
 	
 	private List <String> dictionary;
+	private int numeroErrori;
 	
 	public Model(){
 		dictionary = new ArrayList <String>();
+		numeroErrori = 0;
 	}
 	
 	public void loadDictionary (String language){
 		
-		String file = language + ".txt";
+		dictionary.clear();
+		
+		String file = "rsc/" + language + ".txt";
 		
 		try {
 			FileReader fr = new FileReader(file);
@@ -34,6 +39,62 @@ public class Model {
 		}
 		
 		
+	}
+
+	public String controllaTesto(String testo) {
+		
+		String ris = "";
+		//int j = 0;
+		numeroErrori = 0;
+		
+    	testo = testo.replaceAll(" +", " ");
+		
+		String[] text = testo.split(" ");
+		
+		/*for(int i = 0; i<text.length ; i++){
+			for(j = 0; j<dictionary.size() ; j++)
+				if(text[i].toLowerCase().compareTo(dictionary.get(j).toLowerCase())==0)
+					break;
+			if(j == dictionary.size()){
+				ris += text[i] + "\n";
+				numeroErrori ++;
+			}
+		}*/
+		
+		for(int i = 0; i<text.length ; i++)
+			if(!dictionary.contains(text[i].replaceAll("[ \\p{Punct}]", ""))){
+				ris += text[i] + "\n";
+				numeroErrori ++;
+			}
+		
+		return ris;
+		
+	}
+	
+	public String controlloDicotomico(String testo){
+		
+		String ris = "";
+		
+		int j = 0;
+		
+		numeroErrori = 0;
+		
+    	testo = testo.replaceAll(" +", " ");
+		
+		String[] text = testo.split(" ");
+		
+		Collections.sort(dictionary);
+		
+		/*
+		for(int i = 0 ; i < testo.length() ; i++)
+			while(j== 0 || j==dictionary.size()-1)
+				if(dictionary.get(j).equals(text[i].replaceAll("[ \\p{Punct}]", "")))
+		*/
+		return ris;
+	}
+	
+	public int getNumeroErrori(){
+		return numeroErrori;
 	}
 
 }
